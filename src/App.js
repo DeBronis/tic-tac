18acf88ed274eb73f3d908bcf2875df4793021dd
debroniswarren <debronisw@gmail.com>
2018-10-12 10:16:02 -0600

    Added board to state

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { board: [1,2,3] };
+  }
   render() {
     return (
       <div className="body">
-       
+       <div>{this.state.board[0]}</div>
+       <div>{this.state.board[1]}</div>
+       <div>{this.state.board[2]}</div>
       </div>
     );
   }
